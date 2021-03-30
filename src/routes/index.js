3af0ad2433e69448c50b795176e92efc3cd50a1b
Matthew Faris <mtfaris90@gmail.com>
2021-03-30 17:00:13 -0400

    implement getQuizzes functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,24 @@
-const quizzes = require('../../data/quizzes.json');
+const quizzes = require("../../data/quizzes.json");
 
 /**
  * Returns a list of quizzes with titles and IDs
  */
 async function getQuizzes(req, res, next) {
-  // TODO: Your code goes here
+  try {
+    const data = [];
+    const subjects = await Object.keys(quizzes);
+    for (const subject of subjects) {
+      const {questions, ...result} = await quizzes[subject];
+      data.push(result);
+    }
+    res.send(data);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(404);
+  }
 }
 
-/** 
+/**
  * Returns quiz data for the given ID, omitting the answers
  */
 async function getQuiz(req, res, next) {
